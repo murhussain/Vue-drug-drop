c79c8269a03a50ad89e2d94839037f41664d7d42
Murhussain <husseinmurashi@yahoo.com>
2023-05-14 11:27:37 +0200

    Done with the project goal

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,35 @@
 
 <template>
-  <div class="drop-zone">
-    <div v-for="item in getList(1)" :key="item.id" class="drag-el">
+  <div 
+    class="drop-zone"
+    @drop="onDrop($event, 1)"
+    @dragenter.prevent
+    @dragover.prevent
+  >
+    <div 
+      v-for="item in getList(1)" 
+      :key="item.id" 
+      class="drag-el"
+      draggable="true"
+      @dragstart="startDrag($event, item)"
+    >
       {{item.title}}
     </div>
   </div>
 
-  <div class="drop-zone">
-    <div v-for="item in getList(2)" :key="item.id" class="drag-el">
+  <div 
+    class="drop-zone"
+    @drop="onDrop($event, 2)"
+    @dragenter.prevent
+    @dragover.prevent
+  >
+    <div 
+      v-for="item in getList(2)" 
+      :key="item.id" 
+      class="drag-el"
+      draggable="true"
+      @dragstart="startDrag($event, item)"
+    >
       {{item.title}}
     </div>
   </div>
@@ -28,8 +50,22 @@ export default {
       return items.value.filter(item => item.list === list)
     }
 
+    const startDrag = (event, item) => {
+      event.dataTransfer.dropEffect = "move"
+      event.dataTransfer.effectAllowed = "move"
+      event.dataTransfer.setData("itemID", item.id)
+    } 
+
+    const onDrop = (event, list) => {
+      const itemID = event.dataTransfer.getData("itemID")
+      const item = items.value.find((item) => item.id == itemID)
+      item.list = list
+    }
+
     return {
-      getList
+      getList,
+      startDrag,
+      onDrop
     }
   }
 }
@@ -57,6 +93,10 @@ export default {
   background-color: #3498db;
   padding: 5px;
   color: #fff;
-  margin-button: 10px;
+  margin-bottom: 10px;
+}
+
+.drag-el:nth-last-of-type(1){
+  margin-bottom: 0;
 }
 </style>
